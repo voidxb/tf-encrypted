3971eefcf21eb39c50a6b0d6b007d29e0860c5a6
yann dupis <yann.dupis@gmail.com>
2019-05-08 18:16:57 -0400

    linting examples/logistic

--- examples/logistic/training-single.py
+++ examples/logistic/training-single.py
@@ -1,3 +1,4 @@
+"""Private training on data from a single owner"""
 import tf_encrypted as tfe
 
 from common import DataOwner, ModelOwner, LogisticRegression
@@ -10,15 +11,28 @@ num_batches = (training_set_size // batch_size) * 10
 
 model = LogisticRegression(num_features)
 model_owner = ModelOwner('model-owner')
-data_owner = DataOwner('data-owner', num_features, training_set_size, test_set_size, batch_size)
+data_owner = DataOwner('data-owner',
+                       num_features,
+                       training_set_size,
+                       test_set_size,
+                       batch_size)
 
-x_train, y_train = tfe.define_private_input(data_owner.player_name, data_owner.provide_training_data)
-x_test, y_test = tfe.define_private_input(data_owner.player_name, data_owner.provide_testing_data)
+x_train, y_train = tfe.define_private_input(
+    data_owner.player_name,
+    data_owner.provide_training_data)
+x_test, y_test = tfe.define_private_input(
+    data_owner.player_name,
+    data_owner.provide_testing_data)
 
-reveal_weights_op = tfe.define_output(model_owner.player_name, model.weights, model_owner.receive_weights)
+reveal_weights_op = tfe.define_output(
+    model_owner.player_name,
+    model.weights,
+    model_owner.receive_weights)
 
 with tfe.Session() as sess:
-  sess.run([tfe.global_variables_initializer(), data_owner.initializer], tag='init')
+  sess.run([tfe.global_variables_initializer(),
+            data_owner.initializer],
+           tag='init')
 
   model.fit(sess, x_train, y_train, num_batches)
   model.evaluate(sess, x_test, y_test, data_owner)
